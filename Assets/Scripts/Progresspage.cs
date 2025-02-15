97071651720f563d5d0070df26b9a15f996bcb56
Safiyamahir <safiyamahir03@gmail.com>
2025-02-15 22:10:49 +0530

    Added Script to retrieve Diamonds
    Diamond text in the progress page updated to retrieve diamonds

--- Assets/Scripts/Progresspage.cs
+++ Assets/Scripts/Progresspage.cs
@@ -5,10 +5,16 @@ using TMPro;
 public class ProgressTracker : MonoBehaviour
 {
     public TextMeshProUGUI coinsText; // Assign this in the Inspector
+    public TextMeshProUGUI diamondsText; // Assign in Inspector
+
 
     void Start()
     {
         int savedCoins = PlayerPrefs.GetInt("Coins", 0); // Get stored coins
+        int savedDiamonds = PlayerPrefs.GetInt("Diamond", 0); // Retrieve stored diamonds
+
         coinsText.text = "Coins: " + savedCoins.ToString(); // Display on UI
+        diamondsText.text = "Diamonds: " + savedDiamonds.ToString(); // Display diamonds
+        
     }
 }
