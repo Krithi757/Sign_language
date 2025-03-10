fbb32987f533ef7b5893f0a7916b494d2b60540f
Imeth De Silva <imethdesilva@gmail.com>
2025-03-10 12:29:22 +0530

    updated buttons for the shop store

--- Assets/Scenes/Scripts/SceneNavigator.cs
+++ Assets/Scenes/Scripts/SceneNavigator.cs
@@ -3,22 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneNavigator : MonoBehaviour
 {
-    // Array of scene build indices that we want to navigate between
     private int[] sceneIndices = { 10, 11, 12, 13 };
-    private int currentSceneIndex = 0; // Position in our array, not the actual scene index
+    public GameObject popupPanel;
+    
+    private int currentSceneIndex = 0;
 
     private void Start()
     {
-        // Find the current position in our array based on active scene
+        if (popupPanel != null)
+        {
+            popupPanel.SetActive(false);
+        }
+
         int activeSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
         Debug.Log("Active Scene Build Index: " + activeSceneBuildIndex);
-        
+
+        // Find the current index in the scene list
         for (int i = 0; i < sceneIndices.Length; i++)
         {
             if (sceneIndices[i] == activeSceneBuildIndex)
             {
                 currentSceneIndex = i;
                 Debug.Log("Current Scene Index in Array: " + currentSceneIndex);
+                PlayerPrefs.SetInt("LastSceneIndex", currentSceneIndex); // Store it persistently
                 break;
             }
         }
@@ -27,25 +34,36 @@ public class SceneNavigator : MonoBehaviour
     public void GoToNextScene()
     {
         Debug.Log("GoToNextScene called. Current index before: " + currentSceneIndex);
-        // Increment and wrap around if necessary
+        
         currentSceneIndex = (currentSceneIndex + 1) % sceneIndices.Length;
+        PlayerPrefs.SetInt("LastSceneIndex", currentSceneIndex); // Store new index
         Debug.Log("New index after increment: " + currentSceneIndex + ", Loading scene: " + sceneIndices[currentSceneIndex]);
+
         SceneManager.LoadScene(sceneIndices[currentSceneIndex]);
     }
 
     public void GoToPreviousScene()
     {
         Debug.Log("GoToPreviousScene called. Current index before: " + currentSceneIndex);
-        // Decrement and wrap around if necessary
+
         currentSceneIndex = (currentSceneIndex - 1 + sceneIndices.Length) % sceneIndices.Length;
+        PlayerPrefs.SetInt("LastSceneIndex", currentSceneIndex); // Store new index
         Debug.Log("New index after decrement: " + currentSceneIndex + ", Loading scene: " + sceneIndices[currentSceneIndex]);
+
         SceneManager.LoadScene(sceneIndices[currentSceneIndex]);
     }
-    
-    // Add this method to test the Next button functionality directly
-    public void TestNextButton()
+
+    public void ShowPopUp()
     {
-        Debug.Log("Test Next Button pressed");
-        GoToNextScene();
+        if (popupPanel != null)
+        {
+            popupPanel.SetActive(true);
+        }
+    }
+
+    public void GoToHome()
+    {
+        Debug.Log("GoToHome() called - Switching to Home scene");
+        SceneManager.LoadScene(1);
     }
 }
